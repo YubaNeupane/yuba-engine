73c78592228b2b1f9cc66e5483bee472fb7cf573
YubaNeupane <neopaneyb2@gmail.com>
2023-09-30 13:12:24 -0400

    Added z-index ordering, alpha blending

--- src/main/java/yuba/GameObject.java
+++ src/main/java/yuba/GameObject.java
@@ -8,15 +8,18 @@ public class GameObject {
     private String name;
     private List<Component> components;
     public Transform transform;
+    private int zIndex;
 
     public GameObject(String name){
         this.name = name;
         components = new ArrayList<>();
         this.transform = new Transform();
+        this.zIndex = 0;
     }
 
-    public GameObject(String name, Transform transform){
+    public GameObject(String name, Transform transform, int zIndex){
         this.name = name;
+        this.zIndex = zIndex;
         components = new ArrayList<>();
         this.transform = transform;
     }
@@ -60,6 +63,10 @@ public class GameObject {
         for (Component component : components) {
             component.start();
         }
+    }
+
+    public int getZIndex(){
+        return zIndex;
     }
 
 
